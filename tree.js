dc91bc9816b100826d8b7ad8f45ad7951ec914f0
Sean Spearman <sean.spearman@procore.com>
2017-05-23 22:17:01 -0700

    Use ramda/master to use unreleased mergeDeepWithKey; add passing tests

--- tree.js
+++ tree.js
@@ -1,5 +1,35 @@
+const ramda = require('ramda');
+const util = require('util');
+const treeFromPath = require('./treeFromPath');
+
+function p(msg, printable) {
+  console.log(msg, util.inspect(printable, false, null));
+}
+
+function mergeChildren(key, left, right) {
+  switch(key) {
+    case 'childrenFiles':
+      const result = ramda.concat(left, right);
+      return result;
+      break;
+    case 'childrenDirs':
+      return right;
+      break;
+    default:
+      return right;
+  }
+}
+
 function tree(paths) {
-  return paths;
+  return paths.reduce((tree, path) => {
+    const newTree = treeFromPath(path);
+    const mergedTree = ramda.mergeDeepWithKey(
+      mergeChildren,
+      tree,
+      newTree
+    );
+    return mergedTree;
+  }, {});
 }
 
 module.exports = tree;
--- tree.test.js
+++ tree.test.js
@@ -2,24 +2,157 @@ const tree = require('./tree');
 
 test('', () => {
   const input = [
-    'dir0/ghi.txt',
-    'dir1/dirOmega/apple.txt',
-    'dir1/wvu.txt',
+    'abc.txt',
   ];
   const expected = {
-    dir0: {
+    '/': {
       childrenDirs: {},
-      childrenFiles: ['ghi.txt'],
+      childrenFiles: ['abc.txt'],
     },
-    dir1: {
+  };
+  const result = tree(input);
+  expect(result).toEqual(expected);
+});
+
+test('', () => {
+  const input = [
+    'abc.txt',
+    'dir0/ghi.txt',
+  ];
+  const expected = {
+    '/': {
       childrenDirs: {
-        dirOmega: {
+        dir0: {
           childrenDirs: {},
-          childrenFiles: ['apple.txt'],
+          childrenFiles: ['ghi.txt'],
         },
       },
-      childrenFiles: 'wvu.txt',
-    }
+      childrenFiles: ['abc.txt'],
+    },
+  };
+  const result = tree(input);
+  expect(result).toEqual(expected);
+});
+
+test('', () => {
+  const input = [
+    'abc.txt',
+    'def.txt',
+    'dir0/ghi.txt',
+  ];
+  const expected = {
+    '/': {
+      childrenDirs: {
+        dir0: {
+          childrenDirs: {},
+          childrenFiles: ['ghi.txt'],
+        },
+      },
+      childrenFiles: ['abc.txt', 'def.txt'],
+    },
+  };
+  const result = tree(input);
+  expect(result).toEqual(expected);
+});
+
+test('', () => {
+  const input = [
+    'abc.txt',
+    'def.txt',
+    'dir0/ghi.txt',
+    'dir0/jkl.txt',
+  ];
+  const expected = {
+    '/': {
+      childrenDirs: {
+        dir0: {
+          childrenDirs: {},
+          childrenFiles: ['ghi.txt', 'jkl.txt'],
+        },
+      },
+      childrenFiles: ['abc.txt', 'def.txt'],
+    },
+  };
+  const result = tree(input);
+  expect(result).toEqual(expected);
+});
+
+test('', () => {
+  const input = [
+    'abc.txt',
+    'def.txt',
+    'dir0/ghi.txt',
+    'dir1/jkl.txt',
+  ];
+  const expected = {
+    '/': {
+      childrenDirs: {
+        dir0: {
+          childrenDirs: {},
+          childrenFiles: ['ghi.txt'],
+        },
+        dir1: {
+          childrenDirs: {},
+          childrenFiles: ['jkl.txt'],
+        },
+      },
+      childrenFiles: ['abc.txt', 'def.txt'],
+    },
+  };
+  const result = tree(input);
+  expect(result).toEqual(expected);
+});
+
+test('', () => {
+  const input = [
+    'abc.txt',
+    'def.txt',
+    'dir0/ghi.txt',
+    'dir0/dirAlpha/jkl.txt',
+  ];
+  const expected = {
+    '/': {
+      childrenDirs: {
+        dir0: {
+          childrenDirs: {
+            dirAlpha: {
+              childrenDirs: {},
+              childrenFiles: ['jkl.txt'],
+            },
+          },
+          childrenFiles: ['ghi.txt'],
+        },
+      },
+      childrenFiles: ['abc.txt', 'def.txt'],
+    },
+  };
+  const result = tree(input);
+  expect(result).toEqual(expected);
+});
+
+test('', () => {
+  const input = [
+    'abc.txt',
+    'def.txt',
+    'dir0/ghi.txt',
+    'dir0/dirAlpha/jkl.txt',
+    'dir0/dirAlpha/mno.txt',
+  ];
+  const expected = {
+    '/': {
+      childrenDirs: {
+        dir0: {
+          childrenDirs: {
+            dirAlpha: {
+              childrenDirs: {},
+              childrenFiles: ['jkl.txt', 'mno.txt'],
+            },
+          },
+          childrenFiles: ['ghi.txt'],
+        },
+      },
+      childrenFiles: ['abc.txt', 'def.txt'],
+    },
   };
   const result = tree(input);
   expect(result).toEqual(expected);
