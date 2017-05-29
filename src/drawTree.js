c30aa11e8df42d7807f16b127842df38ef10ad3f
Sean Spearman <sean.spearman@procore.com>
2017-05-28 17:05:06 -0700

    Add first stab at drawTree and make it pass first test

--- src/drawTree.js
+++ src/drawTree.js
@@ -1,4 +1,32 @@
-const drawTree = () => {
+const ramda = require('ramda');
+
+const print = console.log;
+
+const CHILD = '├';
+const LAST_CHILD = '└';
+const HORZ_SPACER = '─';
+const VERT_SPACER = '│';
+const PREFIX_SPACE = `${HORZ_SPACER}${HORZ_SPACER} `;
+
+const isNotEmpty = (x) => {
+  return !ramda.isEmpty(x);
+}
+
+const drawTree = (tree) => {
+  return ramda.keys(tree).reduce((treeStr, key) => {
+    let growingTreeStr = treeStr;
+    growingTreeStr = growingTreeStr.concat(`  ${key}\n`);
+    const dir = tree[key];
+    if (isNotEmpty(dir.childrenDirs)) {
+      drawTree(dir.childrenDirs);
+    }
+    dir.childrenFiles.map((file, i, files) => {
+      const boxChar = i === files.length - 1 ? LAST_CHILD : CHILD;
+      const { status, path } = file;
+      growingTreeStr = growingTreeStr.concat(`${status} ${boxChar}${PREFIX_SPACE}${path}\n`);
+    });
+    return growingTreeStr;
+  }, '');
 };
 
 module.exports = drawTree;
