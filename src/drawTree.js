2dab7b0335dd72dfb7975518181d02cd090aa045
Sean Spearman <sean.spearman@procore.com>
2017-05-29 17:51:29 -0700

    Make two-tiered directory tests pass
    - Need to add logic for when to use VERT_SPACER for directory lines

--- src/drawTree.js
+++ src/drawTree.js
@@ -13,17 +13,23 @@ const isNotEmpty = (x) => {
 }
 
 const drawTree = (tree) => {
+  return _drawTree(tree, 0);
+}
+
+const _drawTree = (tree, depth) => {
   return ramda.keys(tree).reduce((treeStr, key) => {
     let growingTreeStr = treeStr;
-    growingTreeStr = growingTreeStr.concat(`  ${key}\n`);
+    const dirDepthStr = `${CHILD}${HORZ_SPACER}${HORZ_SPACER} `;
+    growingTreeStr = growingTreeStr.concat(`  ${dirDepthStr.repeat(depth)}${key}\n`);
     const dir = tree[key];
     if (isNotEmpty(dir.childrenDirs)) {
-      drawTree(dir.childrenDirs);
+      growingTreeStr = growingTreeStr.concat(_drawTree(dir.childrenDirs, depth + 1));
     }
     dir.childrenFiles.map((file, i, files) => {
-      const boxChar = i === files.length - 1 ? LAST_CHILD : CHILD;
+      const childSymbol = i === files.length - 1 ? LAST_CHILD : CHILD;
       const { status, path } = file;
-      growingTreeStr = growingTreeStr.concat(`${status} ${boxChar}${PREFIX_SPACE}${path}\n`);
+      const depthStr = `${VERT_SPACER}   `.repeat(depth);
+      growingTreeStr = growingTreeStr.concat(`${status} ${depthStr}${childSymbol}${PREFIX_SPACE}${path}\n`);
     });
     return growingTreeStr;
   }, '');
