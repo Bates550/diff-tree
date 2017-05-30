356c8b176efd7d490db9d66f99b0bf8a1b6e5671
Sean Spearman <sean.spearman@procore.com>
2017-05-30 00:02:45 -0700

    Make main.js print the drawn tree

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const exec = require('child_process').exec;
 const tree = require('./src/tree');
 const diffStrToChanges = require('./src/diffStrToChanges');
+const drawTree = require('./src/drawTree');
 const print = require('./src/print');
 
 const diffAgainstBranch = process.argv[2];
 exec(`git diff --name-status ${diffAgainstBranch}`, (err, stdout, stderr) => {
   const changes = diffStrToChanges(stdout);
-  print('g diff --name-status:', stdout);
-  print('changes:', changes);
+  const treeData = tree(changes);
   print('tree(changes):', tree(changes));
+  console.log(drawTree(treeData));
 });
