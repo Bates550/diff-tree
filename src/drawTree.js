35666db9a27433c2f60e6224684026ab14308224
Sean Spearman <sean.spearman@procore.com>
2017-06-07 22:42:57 -0700

    Move file line generation into function

--- src/drawTree.js
+++ src/drawTree.js
@@ -10,15 +10,22 @@ const PREFIX_SPACE = `${HORZ_SPACER}${HORZ_SPACER} `;
 
 const isNotEmpty = (x) => {
   return !ramda.isEmpty(x);
-}
+};
 
 const drawTree = (tree) => {
-  return _drawTree(tree, 0, 1, 0);
-}
+  return _drawTree(tree, 0, 1, 0, true);
+};
 
-const _drawTree = (tree, depth, numSiblingDirs, numSiblingFiles) => {
+const generateFileLine = (file, { isLastChild, isLastItem, depth }) => {
+  childSymbol = isLastChild ? LAST_CHILD : CHILD;
+  const { status, path } = file;
+  const depthStr = `${isLastItem ? ' ' : VERT_SPACER}   `.repeat(depth);
+  return `${status} ${depthStr}${childSymbol}${PREFIX_SPACE}${path}\n`;
+};
+
+const _drawTree = (tree, depth, numSiblingDirs, numSiblingFiles, isParentLastItem) => {
   return ramda.keys(tree).reduce((treeStr, key, i) => {
-    const isLastDir = i + 1 === numSiblingDirs;
+    const isLastDir = i === numSiblingDirs;
     const hasNoSiblingFiles = numSiblingFiles === 0;
     const isLastItem = isLastDir && hasNoSiblingFiles;
     const dir = tree[key];
@@ -30,13 +37,15 @@ const _drawTree = (tree, depth, numSiblingDirs, numSiblingFiles) => {
     growingTreeStr = growingTreeStr.concat(`  ${dirDepthStr.repeat(numDirDepthStrs)}${lastDirDepthStr.repeat(numLastDirDepthStrs)}${key}\n`);
     debugger;
     if (isNotEmpty(dir.childrenDirs)) {
-      growingTreeStr = growingTreeStr.concat(_drawTree(dir.childrenDirs, depth + 1, ramda.keys(dir.childrenDirs).length, dir.childrenFiles.length));
+      growingTreeStr = growingTreeStr.concat(_drawTree(dir.childrenDirs, depth + 1, ramda.keys(dir.childrenDirs).length - 1, dir.childrenFiles.length, isLastItem));
     }
     dir.childrenFiles.map((file, i, files) => {
-      const childSymbol = i === files.length - 1 ? LAST_CHILD : CHILD;
-      const { status, path } = file;
-      const depthStr = `${isLastItem ? ' ' : VERT_SPACER}   `.repeat(depth);
-      growingTreeStr = growingTreeStr.concat(`${status} ${depthStr}${childSymbol}${PREFIX_SPACE}${path}\n`);
+      const fileLine = generateFileLine(file, {
+        depth,
+        isLastChild: i === files.length - 1,
+        isLastItem,
+      });
+      growingTreeStr = growingTreeStr.concat(fileLine);
     });
     return growingTreeStr;
   }, '');
