62e5442dee18601146c209b33535f39d0727d058
Sean Spearman <sean.spearman@procore.com>
2017-06-11 00:48:01 -0700

    Fix import and remove debug printing

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const exec = require('child_process').exec;
 const tree = require('./src/tree');
 const diffStrToChanges = require('./src/diffStrToChanges');
-const drawTree = require('./src/drawTree');
+const { drawTree } = require('./src/drawTree');
 const print = require('./src/print');
 
 const diffAgainstBranch = process.argv[2];
@@ -10,7 +10,6 @@ exec('git symbolic-ref --short HEAD', (err, stdout, stderr) => {
   exec(`git diff --name-status ${diffAgainstBranch}...${thisBranch}`, (err, stdout, stderr) => {
     const changes = diffStrToChanges(stdout);
     const treeData = tree(changes);
-    print('tree(changes):', tree(changes));
     console.log(drawTree(treeData));
   });
 });
