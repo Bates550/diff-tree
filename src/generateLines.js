9fe412250e38887eb9768bcd40f98b07f142a573
Sean Spearman <sean.spearman@procore.com>
2017-06-11 01:15:41 -0700

    Move generateLines tests into their own file

--- src/generateLines.js
+++ src/generateLines.js
@@ -1,6 +1,6 @@
 const { VERT_SPACER, LAST_CHILD, CHILD, PREFIX_SPACE } = require('./constants');
 
-const generateDepthStr = (areAncestorsLastChildren) => {
+const _generateDepthStr = (areAncestorsLastChildren) => {
   return areAncestorsLastChildren.reduce((str, ancestor, i) => {
     if (i === 0) {
       return str;
@@ -13,14 +13,14 @@ const generateDepthStr = (areAncestorsLastChildren) => {
 const generateFileLine = ({ file, isLastChild, areAncestorsLastChildren }) => {
   const childSymbol = isLastChild ? LAST_CHILD : CHILD;
   const { status, path } = file;
-  const depthStr = generateDepthStr(areAncestorsLastChildren);
+  const depthStr = _generateDepthStr(areAncestorsLastChildren);
   return `${status} ${depthStr}${childSymbol}${PREFIX_SPACE}${path}\n`;
 };
 
 const generateDirLine = ({ isLastItem, key, areAncestorsLastChildren }) => {
   const depth = areAncestorsLastChildren.length;
   const lastDirDepthStr = `${isLastItem ? LAST_CHILD : CHILD}${PREFIX_SPACE}`;
-  const dirDepthStr = generateDepthStr(areAncestorsLastChildren);
+  const dirDepthStr = _generateDepthStr(areAncestorsLastChildren);
   const numLastDirDepthStrs = depth > 0 ? 1 : 0;
   const numDirDepthStrs = depth <= 0 ? 0 : depth - 1;
   return `  ${dirDepthStr}${lastDirDepthStr.repeat(numLastDirDepthStrs)}${key}\n`
@@ -29,4 +29,5 @@ const generateDirLine = ({ isLastItem, key, areAncestorsLastChildren }) => {
 module.exports = {
   generateFileLine,
   generateDirLine,
+  _generateDepthStr,
 }
