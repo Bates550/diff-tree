9f748edabaa0c60e3d166464408191b8535a027a
AlahmadiQ8 <m91.alahmadi@gmail.com>
2017-09-16 15:12:35 -0700

    added interactive mode for selecting branches to compare

--- main.js
+++ main.js
@@ -1,26 +1,39 @@
 #!/usr/bin/env node
-
+const program = require('commander');
+const R = require('ramda');
 const exec = require('child_process').exec;
 const tree = require('./src/tree');
 const diffStrToChanges = require('./src/diffStrToChanges');
 const drawTree = require('./src/drawTree');
 const print = require('./src/print');
+const interactive = require('./src/interactive');
 
-const execDiffandDrawTree = (branchB) => {
-  exec(`git diff --name-status ${diffAgainstBranch}...${branchB}`, (err, stdout, stderr) => {
+const execDiffandDrawTree = (branchA, branchB) => {
+  exec(`git diff --name-status ${branchA}...${branchB}`, (err, stdout, stderr) => {
     const changes = diffStrToChanges(stdout);
     const treeData = tree(changes);
     console.log(drawTree(treeData));
   });
 };
 
-const diffAgainstBranch = process.argv[2] || 'master';
-if (process.argv[3] === undefined) {
-  exec('git symbolic-ref --short HEAD', (err, stdout, stderr) => {
-    const checkedOutBranch = stdout.trim();
-    execDiffandDrawTree(checkedOutBranch);
-  });
-} else {
-  const branchB = process.argv[3];
-  execDiffandDrawTree(branchB);
+program
+  .usage('[options] [branchA [branchB]]')
+  .option('-i --interactive', 'Interactive mode', false)
+  .parse(process.argv);
+
+if (program.interactive) {
+  console.log('do interactive');
+  interactive(execDiffandDrawTree);
+} else { 
+  const diffAgainstBranch = R.defaultTo('master', program.args[0]);
+  if (program.args.length <= 1) {
+    exec('git symbolic-ref --short HEAD', (err, stdout, stderr) => {
+      const checkedOutBranch = stdout.trim();
+      execDiffandDrawTree(diffAgainstBranch, checkedOutBranch);
+    });
+  } else {
+    console.log(program.args)
+    const branchB = program.args[1];
+    execDiffandDrawTree(diffAgainstBranch, branchB);
+  }
 }
